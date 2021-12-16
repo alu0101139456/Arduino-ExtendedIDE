fee6a0aa3aa4f45b90934ea8bbfdd45d4cca7d17
Ángel <alu0101139456@ull.edu.es>
2021-12-16 13:58:48 +0000

    Update

--- ExtendedIde.cpp
+++ ExtendedIde.cpp
@@ -2,13 +2,33 @@
 
 
 
-ExtendedIde::ExtendedIde() {
-
-
+ExtendedIde::ExtendedIde(int pin, float freq) {
+  pin_ = pin;
+  freq = freq_;
+  SelectTimerWithPin();
+  prescAndFreq_ = clock_->GetAproximateFreq(freq_);
 }
 
-void ExtendedIde::printValue() {
+void ExtendedIde::PrintParamters() {
+  std::cout << "Pin: " << pin_ << std::endl;
+  std::cout << "Frecuencia: " << freq_ << std::endl;
+  std::cout << "Timer seleccionado: " << clock_->getNumberTimer() << std::endl;
+  std::cout << "Frecuencia más cercana: " << prescAndFreq_.second << std::endl;
+  std::cout << "Prescalar: " << prescAndFreq_.first << std::endl;
+}
 
-    std::cout << a << std::endl;
 
+void ExtendedIde::SelectTimerWithPin() {
+  if(pin_ == 5 || pin_ == 6) {
+    clock_ = new Timer8();
+  }
+  else if (pin_ == 3 || pin_ == 11) {
+    clock_ = new Timer8();
+  }
+  else if (pin_ == 9 || pin_ == 10) {
+    clock_ = new Timer16();
+  }
+  else {
+    std::cerr << "Error: Pin fuera de rango"; 
+  }    
 }
--- ExtendedIde.h
+++ ExtendedIde.h
@@ -1,13 +1,47 @@
 #include <iostream>
+#include "Timer.h"
+#include <vector>
+#include <utility>
 
 class ExtendedIde {
 
+private:
+    int pin_;
+    float freq_;
+    Clock_t *clock_ = nullptr;
+    std::pair<int, float> prescAndFreq_;
+
 public:
 
-    int a = 4;
+    ExtendedIde(int pin, float freq);
+    void SelectTimerWithPin();
+    void PrintParamters();
+    void SelectPrescaler();
+};
 
-    ExtendedIde();
+//Frecuencia Timers
+/*
+Timer0                                                    Pines        5     6
+1    -> 8Mh/1    =  8MHz           8MHz/256 = 31250Hz                OC0B   OC0B   
+8    -> 8Mh/8    =  1MHz           1MHz/256 = 3906Hz          
+64   -> 8Mh/64   = 125KHz     125.000Hz/256 = 488,28Hz    
+256  -> 8Mh/256  = 31,25KH     31.250Hz/256 = 122,07Hz
+1024 -> 8Mh/1024 = 7,8125Hz      7,8125/256 = 0,031Hz 
 
-    void printValue();
+Timer2                                                    Pines       3      11
+1    -> 8Mh/1    =  8MHz           8MHz/256 = 31250Hz               OC2B   OC2B
+8    -> 8Mh/8    =  1MHz           1MHz/256 = 3906Hz      
+64   -> 8Mh/64   = 125KHz     125.000Hz/256 = 488,28Hz
+256  -> 8Mh/256  = 31,25KH     31.250Hz/256 = 122,07Hz
+1024 -> 8Mh/1024 = 7,8125Hz      7,8125/256 = 0,031Hz 
 
-}
+
+//Frecuencia Timer 1 16MH
+
+1 -> 16Mh/1 = 16MHz                  16MHz/256 = 62.500Hz
+8 -> 16Mh/8 = 2MHz                    2MHz/256 = 7.812,5Hz
+64 -> 16Mh/64 = 250KHz              250KHz/256 = 976,5625Hz
+256 -> 16Mh/256 = 62,5KH          62.500Hz/256 = 244,14Hz
+1024 -> 16Mh/1024 = 15,625Hz      15,625Hz/256 = 0.061Hz
+(Entiendo que no hay más convinaciones)
+*/
